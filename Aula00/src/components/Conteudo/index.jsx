2a2482ea17b98ef427573e7c80362b9ee56cac6a
RodrigoRamosDeLima <rodrigoramosdelima.dev@gmail.com>
2024-08-01 22:05:46 -0300

    Aula 00 do professor Rafael

---
+++ Aula00/src/components/Conteudo/index.jsx
@@ -0,0 +1,11 @@
+
+import './index.css'
+function Conteudo() {
+  return (
+    <div className='conteudo-container'>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum nesciunt ab dolorem quasi, nisi facere, dolores mollitia exercitationem, officiis repudiandae iure at qui repellat repellendus quidem ullam magni doloremque vel.</p>
+    </div>
+  )
+}
+
+export default Conteudo
---
+++ Aula00/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import './Header.css'
+
+
+function Header() {
+  return (
+    <div className="header-container">
+
+      <h1> Bye Header</h1>
+       
+
+
+    </div>
+  )
+}
+
+export default Header
